a4e5a2c8ef97a280abcd89e7ea099f9aeb73fea6
Vlad Grebeniuk <Grebeniuk1180406@gmail.com>
2023-10-15 12:51:22 +0300

    Add new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Курсач.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
