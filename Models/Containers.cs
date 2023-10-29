2f8291616bcb370ab1a20d226c9d0924fd793090
Vlad Grebeniuk <Grebeniuk1180406@gmail.com>
2023-10-29 11:42:16 +0200

    feat: add header && some modules

---
+++ Models/Containers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Курсач.Models
+{
+    public class Containers
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public int PersonId { get; set; }
+        public DateTime DataStart { get; set; }
+        public DateTime DataEnd { get; set; }
+        public int currentProgres { get; set; }
+        public int endProgres { get; set; }
+
+
+    }
+}
